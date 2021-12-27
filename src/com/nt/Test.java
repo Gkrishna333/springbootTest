a9951abfabc82c9b30306b6acb6640430e0e6fe9
saikr <saikr@LAPTOP-3LTOB904>
2021-12-27 22:35:15 +0530

    frm 1 removed all

--- src/com/nt/Test.java
+++ src/com/nt/Test.java
@@ -4,8 +4,6 @@ public class Test {
 	
 	public static void main(String[] args) {
 	
-		System.out.println("hello one tewoo");
-		System.out.println("//////////////////////////////");
-		System.out.println(1/5);
+		
 	}
 }
