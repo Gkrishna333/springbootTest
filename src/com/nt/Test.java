35d5f741ab654a85dbd743f99d0cf0a152956935
saikr <saikr@LAPTOP-3LTOB904>
2021-12-27 22:27:35 +0530

    from 1 dev remoced

--- src/com/nt/Test.java
+++ src/com/nt/Test.java
@@ -3,9 +3,7 @@ package com.nt;
 public class Test {
 	
 	public static void main(String[] args) {
-	  int a=21;
-	  int b=8651;
-		System.out.println("hello welcome");
+	
 		System.out.println("hello one tewoo");
 		System.out.println("//////////////////////////////");
 		System.out.println(1/5);
