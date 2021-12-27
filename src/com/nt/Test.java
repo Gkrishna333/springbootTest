80a0a8272542caee27059d2fcdda236ba41d2da3
saikr <saikr@LAPTOP-3LTOB904>
2021-12-27 22:09:54 +0530

    added some code

--- src/com/nt/Test.java
+++ src/com/nt/Test.java
@@ -3,7 +3,8 @@ package com.nt;
 public class Test {
 	
 	public static void main(String[] args) {
-	
+	  int a=21;
 		System.out.println("hello welcome");
+		System.out.println("hello one tewoo");
 	}
 }
